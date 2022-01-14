7c5bc4c9ea79f388fafa5843bcf5297741a1be1d
ninhnv-1216 <nguyen.van.ninh@sun-asterisk.com>
2022-01-14 15:40:05 +0700

    Advanced login

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ApplicationRecord
   before_save :downcase_email
+  attr_accessor :remember_token
+
   validates :name, presence: true,
     length: {maximum: Settings.max_name}
   validates :email, presence: true,
@@ -10,6 +12,36 @@ class User < ApplicationRecord
     if: :password
 
   has_secure_password
+
+  class << self
+    def digest string
+      cost = if ActiveModel::SecurePassword.min_cost
+               BCrypt::Engine::MIN_COST
+             else
+               BCrypt::Engine.cost
+             end
+      BCrypt::Password.create string, cost: cost
+    end
+
+    def new_token
+      SecureRandom.urlsafe_base64
+    end
+  end
+
+  def remember
+    self.remember_token = User.new_token
+    update remember_digest: User.digest(remember_token)
+  end
+
+  def authenticated? remember_token
+    return false unless remember_token
+
+    BCrypt::Password.new(remember_digest).is_password? remember_token
+  end
+
+  def forget
+    update_attribute :remember_digest, nil
+  end
 
   private
   def downcase_email
